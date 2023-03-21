ef8b3290b43fa546ab1e4d5090a721ac467bac8b
FreddyKly <Frederik.Kliemt@web.de>
2023-03-21 15:01:46 +0100

    joinGame workflow complete

--- Frontend/src/components/EnterGame.vue
+++ Frontend/src/components/EnterGame.vue
@@ -1,48 +1,103 @@
 <template>
     <q-page>
-      <div class="column fixed-center">
-        <q-btn class="q-ma-md" color="primary" no-wrap label="Create" size="1.5rem" icon="add" v-on:click="createGame()"/>
-        <q-btn class="q-ma-md" color="primary" no-wrap label="Join" size="1.5rem" icon="group" v-on:click="joinGame()"/>
-      </div>
+        <div class="column fixed-center">
+            <q-btn class="q-ma-md" color="primary" no-wrap label="Create" size="1.5rem" icon="add"
+                v-on:click="createGame()" />
+            <q-btn class="q-ma-md" color="primary" no-wrap label="Join" size="1.5rem" icon="group"
+                v-on:click="join = true" />
+
+            <!-- Popup Dialog -->
+            <q-dialog v-model="join" persistent>
+                <q-card style="min-width: 80vw;">
+                    <q-card-section>
+                        <div class="text-h6">Game-ID</div>
+                    </q-card-section>
+
+                    <q-card-section class="q-pt-none">
+                        <q-input dense v-model="gameID" autofocus @keyup.enter="joinGame()" />
+                    </q-card-section>
+
+                    <q-card-actions class="text-primary">
+                        <q-btn flat label="Cancel" v-close-popup />
+                        <q-btn flat label="Join" v-on:click="joinGame()"/>
+                    </q-card-actions>
+                </q-card>
+            </q-dialog>
+
+            <q-dialog v-model="invalidGameID" persistent transition-show="scale" transition-hide="scale">
+                <q-card style="width: 50vw">
+                    <q-card-section>
+                        <div class="text-h6">Game-ID does not exist</div>
+                    </q-card-section>
+
+                    <q-card-actions>
+                        <q-btn text-color="primary" flat label="OK" v-close-popup />
+                    </q-card-actions>
+                </q-card>
+            </q-dialog>
+        </div>
     </q-page>
-  </template>
+</template>
   
-  <script>
-  import { defineComponent } from 'vue'
-  import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
-  import { setDoc, doc } from 'firebase/firestore'
-  import { useRouter } from 'vue-router';
-  import { db } from 'src/firebaseConfig'
-  
-  export default defineComponent({
+<script>
+import { defineComponent, ref } from 'vue'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { setDoc, doc, query, collection, where, getDocs } from 'firebase/firestore'
+import { useRouter } from 'vue-router';
+import { db } from 'src/firebaseConfig'
+import router from 'src/router';
+
+export default defineComponent({
     name: 'EnterGameComponent',
-    
-  
+
+
     async setup() {
-      const auth = getAuth()
-      console.log(auth.currentUser)
-        
+        var join = ref(false)
+        var invalidGameID = ref(false)
+        var gameID = ref('')
+        const auth = getAuth()
+        console.log(auth.currentUser)
 
-      await setDoc(doc(db, "user", auth.currentUser.uid), {
-        e_mail: auth.currentUser.email,
-        google_id: auth.currentUser.uid,
-        group_id: 1,
-        guesses: [],
-        total_points: 0,
-        username: auth.currentUser.displayName,
-      })
+        async function createGame() {
+            console.log('Create Game')
+            await setDoc(doc(db, "user", auth.currentUser.uid), {
+                e_mail: auth.currentUser.email,
+                google_id: auth.currentUser.uid,
+                game_id: 'g' + auth.currentUser.uid,
+                guesses: [],
+                total_points: 0,
+                username: auth.currentUser.displayName,
+            })
+            router.push('/rankings')
+        }
 
+        async function joinGame() {
+            console.log('Join Game')
+            console.log(join.value)
+            const userQ = query(collection(db, "user"), where("game_id", "==", gameID.value));
+            const userSnap = await getDocs(userQ);
+            console.log(userSnap)
+            if (!userSnap.empty) {
+                console.log('exists')
+                await setDoc(doc(db, "user", auth.currentUser.uid), {
+                    e_mail: auth.currentUser.email,
+                    google_id: auth.currentUser.uid,
+                    game_id: 'g' + gameID.value,
+                    guesses: [],
+                    total_points: 0,
+                    username: auth.currentUser.displayName,
+                })
+                join.value = false
+                router.push('/rankings')
+            } else {
+                console.log('didnt exist')
+                invalidGameID.value = true
+            }
 
-      async function createGame() {
-        console.log('Create Game')
-      }
+        }
 
-      async function joinGame() {
-        console.log('Join Game')
-      }
-      
-      return { createGame, joinGame }
+        return { createGame, joinGame, join, gameID, invalidGameID }
     }
-  })
-  </script>
+})
+</script>
   
